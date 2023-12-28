2b0a481b0d72c72bab523f7ece238a40b1b3586b
systemx5 <154720925+systemx5@users.noreply.github.com>
2023-12-28 08:30:50 +0530

    Update config.py
    Signed-off-by: systemx5 <154720925+systemx5@users.noreply.github.com>

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SESSION = getenv("SESSION", None)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MAHTOxOFFICIAL")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MAHTOxOFFICIAL")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6866756579").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
